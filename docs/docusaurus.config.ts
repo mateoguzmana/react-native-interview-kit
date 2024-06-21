7c181718920cbc3fc32a26b6287871f99591bd2b
Mateo Guzmán <info@mateoguzman.net>
2024-06-21 17:39:22 +0200

    docs: tweaks

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://mateoguzmana.github.io',
+  url: 'https://mateoguzmana.github.io/react-native-interview-kit',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/react-native-interview-kit/',
